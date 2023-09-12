ecd82a3aedd3cae0ddac1d507da84c45c6305c13
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-09-12 22:15:05 +0500

    Add files via upload

---
+++ SMIT attendance application/config.js
@@ -0,0 +1,16 @@
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAlwkQIVwpVmnwabgOXQ6CrGTGJXROXZ2o",
+//   authDomain: "smit-attendance-app-a633f.firebaseapp.com",
+//   projectId: "smit-attendance-app-a633f",
+//   storageBucket: "smit-attendance-app-a633f.appspot.com",
+//   messagingSenderId: "218351979162",
+//   appId: "1:218351979162:web:ad8e3ed552cd743d092d23",
+//   measurementId: "G-VFR1M47TZ9"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
